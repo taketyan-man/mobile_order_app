031142b402a90190c5607c34494b5107770bc4c2
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-09-14 18:28:17 +0900

    react setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-console.log('Hello from JavaScript!');
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloReact from "./components/HelloReact";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <HelloReact />,
+    document.getElementById("react-root")
+  );
+});
---
+++ app/javascript/components/HelloReact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloReact = () => {
+  return <div>Hello, React with Importmap!</div>;
+};
+
+export default HelloReact;
